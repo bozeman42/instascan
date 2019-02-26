4ca658d0a3c1c7dfc39cbd87ef79907ea0117a51
Aaron Kvarnlov-Leverty <bozeman42@gmail.com>
2019-02-25 23:58:11 -0600

    replace zxing.js with npm zxing library

--- src/scanner.js
+++ src/scanner.js
@@ -1,5 +1,5 @@
 const EventEmitter = require('events');
-const ZXing = require('./zxing')();
+const ZXing = require('@zxing/library')
 const Visibility = require('visibilityjs');
 const StateMachine = require('fsm-as-promised');
 
